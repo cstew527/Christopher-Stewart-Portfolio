680f1cfcda41899ae3f76717eb98c5ebe91c76b5
cstew527 <christopherstewart05@gmail.com>
2022-11-08 00:43:09 -0500

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Christopher-Stewart-Portfolio
+
+# Project 1
+
+This is a portfolio site for myself built with HTML, JavaScript, CSS, and JQuery and hosted on Netlify.
+
+
+## 🚀 About Me
+I'm a full stack developer...
+
+
+## 🔗 Links
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://git.generalassemb.ly/cstew527)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/christopher-kareem-stewart/)
+
+
+## Features
+- Live previews
+- Fullscreen mode
+- Cross platform
+
--- app.js
+++ app.js
@@ -1,55 +1,86 @@
 $(() => {
+    // Nav bar
     $(".bio").hide()
     $(".resume").hide()
     $(".projects").hide()
-    $(".links").hide()
 
     const showHome = () => {
         $(".home").slideDown(1000)
+        $(".header").slideDown(1000)
         $(".bio").slideUp(1000)
         $(".resume").slideUp(1000)
         $(".projects").slideUp(1000)
-        $(".links").slideUp(1000)
     }
 
     const showBio = () => {
         $(".home").slideUp(1000)
+        $(".header").slideDown(1000)
         $(".bio").slideDown(1000)
         $(".resume").slideUp(1000)
         $(".projects").slideUp(1000)
-        $(".links").slideUp(1000)
     }
 
     const showResume = () => {
         $(".home").slideUp(1000)
+        $(".header").slideDown(1000)
         $(".bio").slideUp(1000)
         $(".resume").slideDown(1000)
         $(".projects").slideUp(1000)
-        $(".links").slideUp(1000)
     }
 
     const showProjects = () => {
         $(".home").slideUp(1000)
+        $(".header").slideUp(1000)
         $(".bio").slideUp(1000)
         $(".resume").slideUp(1000)
         $(".projects").slideDown(1000)
-        $(".links").slideUp(1000)
-    }
-
-    const showLinks = () => {
-        $(".home").slideUp(1000)
-        $(".bio").slideUp(1000)
-        $(".resume").slideUp(1000)
-        $(".projects").slideUp(1000)
-        $(".links").slideDown(1000)
     }
 
     $("#home").on('click', showHome)
     $("#bio").on('click', showBio)
     $("#resume").on('click', showResume)
     $("#projects").on('click', showProjects)
-    $("#links").on('click', showLinks)
+})
 
+$(() => {
+    // Project carousel
+    $(".project-hangman").hide()
+    $(".project-space-battle").hide()
+    $(".project-color-compare").hide()
+
+    let currentImage = 0
+    let numOfImages = $(".image-container").children().length - 1
+
+    $('.next-button').on('click', () => {
+        $('.image-container').children().eq(currentImage).css('display', 'none')
+        for (let i = 0; i < numOfImages + 1; i++) {
+            $('.projects-container').children().eq(i).slideUp(1000)
+        }
+        if (currentImage < numOfImages) {
+            currentImage++
+        } else {
+            currentImage = 0
+        }
+        $('.image-container').children().eq(currentImage).css('display', 'block')
+        $('.projects-container').children().eq(currentImage).slideDown(1000)
+    })
+
+    $('.previous-button').on('click', () => {
+        $('.image-container').children().eq(currentImage).css('display', 'none')
+        for (let i = 0; i < numOfImages + 1; i++) {
+            $('.projects-container').children().eq(i).slideUp(1000)
+        }
+        if (currentImage > 0) {
+            currentImage--
+        } else {
+            currentImage = numOfImages
+        }
+        $('.image-container').children().eq(currentImage).css('display', 'block')
+        $('.projects-container').children().eq(currentImage).slideDown(1000)
+    })
+})
+
+$(() => {
     // Landscaper Game
     const player = {
         items: [
@@ -80,4 +111,43 @@ $(() => {
             },
         ]
     }
+
+    let currentItem = 0
+    let playersBalance = 0
+
+    $('#playersBalance').text('Current Balance: ' + playersBalance)
+    $('#currentItem').text('Current Item:' + player.items[0].item)
+
+    $('#cutGrass').on('click', function() {
+        playersBalance += player.items[currentItem].earning
+        $('#playersBalance').text('Current Balance: ' + playersBalance)
+        if (playersBalance >= 1000 && currentItem == 4) {
+            alert("You have finished the game!")
+        }
+    })
+
+    $('#buyItem').on('click', function() {
+        if (currentItem == 4) {
+            alert("You can't upgrade anymore!")
+        } else if (playersBalance >= player.items[currentItem + 1].price) {
+            playersBalance -= player.items[currentItem + 1].price
+            currentItem += 1
+            $('#playersBalance').text('Current Balance: ' + playersBalance)
+            $('#equippedItem').text('Current Item: ' + player.items[currentItem].item)
+        } else if (playersBalance < player.items[currentItem + 1].price) {
+            alert("You don't have enough money to buy!")
+        }
+    })
+})
+
+// Hangman
+$(() => {
+
+})
+
+//Resume button
+$(() => {
+    $('.btn').on('click', function() {
+        window.location.href = "documents/resume.pdf"
+    })
 })
